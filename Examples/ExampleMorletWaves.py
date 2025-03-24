78ce5c07ad89c69612c99cea82f182c21ef3fa5d
DESKTOP-4G4GNTG\onwij <onwijzebackup@gmail.com>
2025-03-24 15:21:40 +0100

    Path comment for all examples

--- Examples/ExampleMorletWaves.py
+++ Examples/ExampleMorletWaves.py
@@ -1,3 +1,10 @@
+# # Add the project root directory to the Python path when working with source code, 
+# # not necessary when package is installed
+# import sys
+# import os
+# path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# sys.path.insert(0, path )
+# print(path)
 #%%
 from WaveSpace.Utils import ImportHelpers
 from WaveSpace.Decomposition import Morlet as mw
--- Examples/ExampleWaveActivity.py
+++ Examples/ExampleWaveActivity.py
@@ -1,3 +1,11 @@
+# # Add the project root directory to the Python path when working with source code, 
+# # not necessary when package is installed
+# import sys
+# import os
+# path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# sys.path.insert(0, path )
+# print(path)
+
 #%%
 from WaveSpace.WaveAnalysis import WaveActivity as wa
 from WaveSpace.Utils import ImportHelpers
