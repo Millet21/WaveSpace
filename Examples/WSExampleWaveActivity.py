e8e29b4672cf705ab81b534b52d577a8a47b2377
kpetras <31351649+kpetras@users.noreply.github.com>
2025-07-02 19:18:34 +0200

    fixed waveActivity + demo

--- Examples/WSExampleWaveActivity.py
+++ Examples/WSExampleWaveActivity.py
@@ -11,13 +11,67 @@ print(path)
 from WaveSpace.WaveAnalysis import WaveActivity as wa
 from WaveSpace.Utils import ImportHelpers
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
 #%%
 
 waveData = ImportHelpers.load_wavedata_object("ExampleData/Output/ComplexData")
 
-# %%
-freqRange = (5,20)
-freqlist = np.arange(freqRange[0], freqRange[1], 2)
+# %% Here we look at the spatial basis functions of our data. We index into the dataBucket to use only a subset of trials (of the same conditon) and only the frequency of interest
+nBases=3
+dataInd= (slice(0,1),slice(10,12),slice(None),slice(None),slice(None))
+wa.find_wave_activity(waveData, dataBucketName="AnalyticSignal", dataInd=dataInd, nBases=nBases)
 
-wa.find_wave_activity(waveData, freqlist, dataBucketName="AnalyticSignal", nBases=3)
+bases = waveData.get_data('Bases')
+
+fig, axs = plt.subplots(1, nBases, figsize=(nBases*6, 6))
+if nBases == 1:
+    axs = [axs]  
+for b in range(nBases):
+    im = axs[b].imshow(
+        np.angle(bases[:, :, b]),
+        cmap='hsv',
+        vmin=-np.pi,
+        vmax=np.pi,
+        origin='lower',
+        aspect='auto'
+    )
+    axs[b].set_title(f'wave map {b+1}')
+    axs[b].set_xlabel('posy')
+    axs[b].set_ylabel('posx')
+    fig.colorbar(im, ax=axs[b], fraction=0.046, pad=0.04, label='Phase (rad)')
+
+plt.tight_layout()
+plt.show()
+
+#%% alternatively, we cann calculate the bases on all data at once, and sort out the weights later:
+nBases=5
+dataInd= None
+wa.find_wave_activity(waveData, dataBucketName="AnalyticSignal", dataInd=dataInd, nBases=nBases)
+
+bases = waveData.get_data('Bases')
+
+fig, axs = plt.subplots(1, nBases, figsize=(nBases*6, 6))
+if nBases == 1:
+    axs = [axs]  
+for b in range(nBases):
+    im = axs[b].imshow(
+        np.angle(bases[:, :, b]),
+        cmap='hsv',
+        vmin=-np.pi,
+        vmax=np.pi,
+        origin='lower',
+        aspect='auto'
+    )
+    axs[b].set_title(f'wave map {b+1}')
+    axs[b].set_xlabel('posy')
+    axs[b].set_ylabel('posx')
+    fig.colorbar(im, ax=axs[b], fraction=0.046, pad=0.04, label='Phase (rad)')
+
+plt.tight_layout()
+plt.show()
+
+# the bases have changed to express linear combinations of all the waves we put in. 
+
+
--- WaveSpace/WaveAnalysis/WaveActivity.py
+++ WaveSpace/WaveAnalysis/WaveActivity.py
@@ -2,23 +2,72 @@ import numpy as np
 import WaveSpace.Utils.HelperFuns as hf
 import WaveSpace.Utils.WaveData as wd
 
-def find_wave_activity(waveData, freqList, dataBucketName="ComplexPhaseData", nBases=3, ):
-    waveData.ActiveDataBucket = dataBucketName
+def find_wave_activity(waveData, dataBucketName=None, dataInd = None, nBases=3):
+    """Identifies dominant traveling wave patterns (spatial bases) in complex-valued waveData
+    via singular value decomposition on the data covariance 
+    Inputs:
+    waveData : WaveData object        
+    dataBucketName : str, optional
+        which data bucket to use
+    dataInd : tuple or None, optional
+        Optional index tuple to select a subset of the data (e.g., (slice(0,1), slice(10,12), ...)). 
+        Needs to result in the same number of dimensions as full data. So use slices for singular dimensions. 
+        If None, uses all data.
+    nBases : int, optional
+        The number of spatial bases to extract (default is 3).
+
+    Adds data buckets to waveData:
+    Bases : Complex spatial bases (channels x bases).
+    Fit :   fit per trial and time ((freq) trials x time).
+    betas : weights ((freq) trials x time x bases).
+
+    References
+    ----------
+    - https://github.com/ScaleSymmetry/Traveling-wave-analysis https://doi.org/10.1371/journal.pone.0148413
+          https://doi.org/10.1371/journal.pcbi.1007316"""
+    
+    #sanity checks:
+    if  dataBucketName == "":
+        dataBucketName =  waveData.ActiveDataBucket
+    else:
+        waveData.set_active_dataBucket(dataBucketName)
+
     hf.assure_consistency(waveData)
     complexPhaseData = waveData.get_data(dataBucketName)
+    if dataInd:
+        complexPhaseData = complexPhaseData[dataInd]
     origDimord = waveData.DataBuckets[dataBucketName].get_dimord()
     origShape = complexPhaseData.shape
     desiredDimord = "trl_chan_time"
     hasBeenReshaped, complexPhaseData =  hf.force_dimord(complexPhaseData, origDimord , desiredDimord)
-    nTrials, nChannels, nTime = complexPhaseData.shape
     # Make complex valued Phase/magnitude Timeseries per freq
     #reshape to (trial, time, channel)
     phi= np.transpose(complexPhaseData, (0, 2, 1))
     bases, fit, betas = c_TW_bases_betas(phi,nBases=nBases)
     chan_names = waveData.get_channel_names()
-    basesBucket = wd.DataBucket(bases,"Bases","chan_trl", chan_names)
-    fitBucket = wd.DataBucket(fit,"Fit","trl_chan", chan_names)
-    betasBucket = wd.DataBucket(betas,"betas","trl_chan_beta", chan_names)
+    if hasBeenReshaped:
+        splitDimensions = origDimord.split("_")
+        if "chan" in splitDimensions:
+            nGroupDimensions = splitDimensions.index("chan")
+            channelShape = origShape[nGroupDimensions]
+        elif "posx" in splitDimensions:
+            nGroupDimensions = splitDimensions.index("posx")
+            channelShape = origShape[nGroupDimensions:nGroupDimensions+2]
+
+        groupDimensions = splitDimensions[0:nGroupDimensions]
+        groupDimSizes = origShape[:len(groupDimensions)]
+        multi_indices  = np.array(np.unravel_index(np.arange(complexPhaseData.shape[0]), groupDimSizes)).T
+        
+        bases = np.reshape(bases, (*channelShape, bases.shape[-1]))
+        basesBucket = wd.DataBucket(bases,"Bases","posx_posy_base", chan_names)
+        fit = np.reshape(fit,(*groupDimSizes, fit.shape[-1]))
+        fitBucket = wd.DataBucket(fit,"Fit", ("_").join(groupDimensions) +"_time", chan_names)
+        betas = np.reshape(betas,(*groupDimSizes, betas.shape[-2], betas.shape[-1]))
+        betasBucket = wd.DataBucket(betas,"betas",("_").join(groupDimensions) +"_time_beta", chan_names)
+    else:
+        basesBucket = wd.DataBucket(bases,"Bases","chan_base", chan_names)
+        fitBucket = wd.DataBucket(fit,"Fit","trl_time", chan_names)
+        betasBucket = wd.DataBucket(betas,"betas","trl_time_beta", chan_names)
     waveData.add_data_bucket(basesBucket)
     waveData.add_data_bucket(fitBucket)
     waveData.add_data_bucket(betasBucket)
@@ -29,7 +78,6 @@ def c_TW_bases_betas(phi_cts,nBases=3):
     phi_cent = phi_Cs - phi_Cs.mean(0)
     COV = phi_cent.T.conj()@phi_cent
     u,s,vh = np.linalg.svd(COV)
-    print(100.0*s[:nBases]/s.sum())
     bases_sb = vh[:nBases].T
     betas_Cb = phi_Cs.dot(bases_sb)
     model_Cs = np.exp(1j*np.angle(bases_sb.dot(betas_Cb.T).T))
